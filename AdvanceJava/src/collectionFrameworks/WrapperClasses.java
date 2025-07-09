55e8d03c3f01759a927eb024777d25282896b85e
yashvikaushik <yashvikaushik2525@gmail.com>
2025-07-09 12:30:54 +0530

    Wrapper classes

---
+++ AdvanceJava/src/collectionFrameworks/WrapperClasses.java
@@ -0,0 +1,11 @@
+package collectionFrameworks;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        Integer i=new Integer(10);
+        int j=i;
+        System.out.println("the autoboxing: "+i);
+        System.out.println("the auto unboxing: "+j);
+    }
+    
+}
